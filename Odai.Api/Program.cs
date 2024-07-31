20c697dd7803961336108a9830280840ec0de409
OdaiAlqutami98 <132313889+OdaiAlqutami98@users.noreply.github.com>
2024-07-31 16:40:40 -0700

    DependencyInjection

--- Odai.Api/Program.cs
+++ Odai.Api/Program.cs
@@ -1,3 +1,5 @@
+using Odai.Logic;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
--- Odai.Logic/DependencyInjection.cs
+++ Odai.Logic/DependencyInjection.cs
@@ -1,18 +1,45 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Odai.DataModel;
+using Odai.Domain;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace Odai.Logic
 {
-    public class DependencyInjection
+    public static class DependencyInjection
     {
-        public DependencyInjection(IServiceCollection services,IConfiguration config)
+        public static IServiceCollection AddInfrastructure( this IServiceCollection services, IConfiguration config)
         {
             services.AddDbContext<OdaiDbContext>(option =>
             {
                 option.UseSqlServer(config.GetConnectionString("DefaultConnection"));
             });
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<OdaiDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddAuthentication(option =>
+            {
+                option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = config["JwtSettings:Issuer"],
+                    ValidateAudience = false,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["JwtSettings:Key"]))
+                };
+            });
+            return services;
         }
     }
 }
