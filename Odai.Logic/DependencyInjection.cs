484c550626cdd17765a9302b0c710645fd16ea2d
OdaiAlqutami98 <132313889+OdaiAlqutami98@users.noreply.github.com>
2024-08-22 18:41:17 -0700

    done

--- Odai.Logic/DependencyInjection.cs
+++ Odai.Logic/DependencyInjection.cs
@@ -4,14 +4,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Odai.DataModel;
-using Odai.Domain;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-
-using Odai.Logic.Common.Interface;
-using Odai.Logic.Common;
-using Microsoft.Extensions.Options;
-
 
 namespace Odai.Logic
 {
@@ -24,7 +16,6 @@ namespace Odai.Logic
             {
                 option.UseSqlServer(IdentityConnString, builder => builder.MigrationsAssembly(typeof(OdaiDbContext).Assembly.FullName));
             });
-
             services.AddIdentity<ApplicationUser, IdentityRole<Guid>>(options =>
             {
                 options.User.RequireUniqueEmail = true;
@@ -35,8 +26,6 @@ namespace Odai.Logic
                 options.Password.RequireLowercase = false;
                 options.Password.RequireUppercase = false;
             }).AddEntityFrameworkStores<OdaiDbContext>()
-        
-                .AddEntityFrameworkStores<OdaiDbContext>()
                 .AddDefaultTokenProviders();
 
             services.AddAuthentication(option =>
@@ -56,14 +45,6 @@ namespace Odai.Logic
                     IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["JwtSettings:Key"]))
                 };
             });
-
-            services.AddTransient<IIdentityService, IdentityService>();
-            services.AddScoped<UserManager<ApplicationUser>>();
-            services.AddScoped<SignInManager<ApplicationUser>>();
-           
-
-
-            return services;
         }
     }
 }
